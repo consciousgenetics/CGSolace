a2bdde33684d64bca27f9d4a2000660caa94d721
Your Name <mhal236@uky.edu>
2025-02-28 12:30:57 -0500

    asd

--- src/modules/home/components/product-grid/index.tsx
+++ src/modules/home/components/product-grid/index.tsx
@@ -38,7 +38,7 @@ export default function ProductGrid() {
           {/* Strains Image - Top Right */}
           <div className="relative h-full">
             <Image
-              src="/images/product-packs/Special packs.png"
+              src="/images/product-packs/special-packs.png"
               alt="Collection of colorful strain seed packets"
               fill
               className="object-cover"
@@ -50,7 +50,7 @@ export default function ProductGrid() {
           {/* Seeds Packaging Image - Bottom Left */}
           <div className="relative h-full">
             <Image
-              src="/images/product-packs/Conscious Genetics  Grinder Merch Pack.png"
+              src="/images/product-packs/cg-grinder.png"
               alt="Person packaging seed packets"
               fill
               className="object-cover"
