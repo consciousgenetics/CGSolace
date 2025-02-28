14df26f47e06f4a9b56aa1f4cbd302fbbd8bf1c7
Your Name <mhal236@uky.edu>
2025-02-28 12:53:22 -0500

    asd

--- src/modules/common/icons/conscious-genetics-logo.tsx
+++ src/modules/common/icons/conscious-genetics-logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export const ConsciousGeneticsLogo = ({ 
   className, 
-  width = 200, 
-  height = 80,
+  width = 400,
+  height = 160,
   ...props 
 }: Omit<ImageProps, 'src' | 'alt'>) => {
   return (
