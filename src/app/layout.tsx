28dfe27d9b6575fe1c1dc1f4dfba8ad20b7c91f6
Your Name <mhal236@uky.edu>
2025-02-28 10:06:46 -0500

    Fix horizontal overflow issues by adding overflow-x-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,18 +13,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="overflow-x-hidden">
+      <body suppressHydrationWarning className="overflow-x-hidden">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <div className="text-basic-primary">
+          <div className="text-basic-primary overflow-x-hidden">
             <ProgressBar />
             <Toaster position="bottom-right" offset={65} closeButton />
-            <main className="relative">{props.children}</main>
+            <main className="relative overflow-x-hidden">{props.children}</main>
           </div>
         </ThemeProvider>
       </body>
