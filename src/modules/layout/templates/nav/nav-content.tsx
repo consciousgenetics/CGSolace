7357e34bb21ac14dcea857bd9213c7953ca747d9
Your Name <mhal236@uky.edu>
2025-02-28 13:43:11 -0500

    asd

--- src/modules/layout/templates/nav/nav-content.tsx
+++ src/modules/layout/templates/nav/nav-content.tsx
@@ -55,7 +55,7 @@ export default function NavContent(props: any) {
         })}
       >
         <LocalizedClientLink href="/">
-          <ConsciousGeneticsLogo className="h-20 medium:h-24" />
+          <ConsciousGeneticsLogo className="h-28 medium:h-32" />
         </LocalizedClientLink>
       </Box>
       {!isSearchOpen && (
