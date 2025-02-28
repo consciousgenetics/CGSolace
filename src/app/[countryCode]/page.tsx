e97c1b1e12d46b7e139b571a0a6bbf9c5f7aef7b
Your Name <mhal236@uky.edu>
2025-02-28 07:57:48 -0500

    Fix: Add use client directive to all components

--- src/app/[countryCode]/page.tsx
+++ src/app/[countryCode]/page.tsx
@@ -1,3 +1,5 @@
+'use server'
+
 import { Metadata } from "next"
 import HeroSection from "../components/HeroSection"
 import ProductShowcase from "../components/ProductShowcase"
--- src/app/components/CustomerReviews.tsx
+++ src/app/components/CustomerReviews.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 const reviews = [
   {
     id: 1,
